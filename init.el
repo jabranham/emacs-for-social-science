47fd964564e774ebca0fc228c5fbc1d6969e41ce
Alex Branham <branham@utexas.edu>
2017-08-29 21:04:26 -0500

    Put package.el customization in init.el, before package-initialize

--- init.el
+++ init.el
@@ -15,8 +15,29 @@
 
 (if (version< emacs-version "24.5.1"); Minimum version is emacs 24.5.1
     (error "Your Emacs is too old -- this config requires v%s or higher"))
+
+;; package.el customization - here we set up how Emacs handles packages!
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;; Windows has issues using https:// instead of http:// so we want to
+;; set the package locations to use the secure connection when they
+;; can:
+(cond
+ ((string-equal system-type "windows-nt") ; Microsoft Windows
+  (progn
+    (setq package-archives
+          '(("melpa" . "http://melpa.org/packages/")
+            ("elpa" . "http://elpa.gnu.org/packages/")))))
+ ((or (string-equal system-type "gnu/linux") ; Linux
+      (string-equal system-type "darwin")) ; & Mac OS X
+  (progn
+    (setq package-archives
+          '(("melpa" . "https://melpa.org/packages/")
+            ("elpa" . "https://elpa.gnu.org/packages/"))))))
 (package-initialize)
-(require 'org)
+
+;; Load the init-emacs.org file:
 (org-babel-load-file (concat user-emacs-directory "init-emacs.org"))
 
 ;;; init.el ends here
